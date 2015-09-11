baaba7d6b56adaa7ae123d25dfdb2b30e461751b
busayo <prosperotemuyiwa@gmail.com>
2015-09-11 06:22:03 +0100

    [Feat] Add User and Friends relationship

--- app/User.php
+++ app/User.php
@@ -64,5 +64,22 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         return "https://www.gravatar.com/avatar/{{ md5($this->email) }}?d=mm&s=40";
     }
 
+    public function friendsOfMine()
+    {
+        return $this->belongsToMany('Tinder\User', 'friends', 'user_id', 'friend_id');
+    }
+
+    public function friendOf()
+    {
+        return $this->belongsToMany('Tinder\User', 'friends', 'friend_id', 'user_id');
+    }
+
+    public function friends()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted', true)
+                        ->get()
+                        ->merge($this->friendOf()->wherePivot('accepted', true)->get());
+    }
+
 
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,6 +6,16 @@
             @include('user.partials.userblock')
             <hr>
         </div>
-        <div class="col-lg-4 col-lg-offset-3"
+        <div class="col-lg-4 col-lg-offset-3">
+            <h4>{{ $user->getFirstNameOrUsername() }}'s  friends</h4>
+
+            @if (! $user->friends()->count())
+                Sorry, You have no friends
+            @else
+                @foreach( $user->friends() as $user)
+                    @include('user.partials.userblock')
+                @endforeach
+            @endif
+        </div>
     </div>
 @stop
